a33f73906f68ded07acbfad0521f70ca59c256c2
Kian Ahrabian <kian.ahrabian@mail.mcgill.ca>
2019-12-02 00:12:36 -0500

    Fix handle_exception to return the results

--- src/common/decorator.py
+++ src/common/decorator.py
@@ -4,10 +4,11 @@ logger = logging.getLogger(__name__)
 
 
 def handle_exception(fn: callable) -> callable:
-    def wrapper(*args: list, **kwargs: dict) -> None:
+    def wrapper(*args: list, **kwargs: dict) -> object:
         try:
-            fn(*args, **kwargs)
+            return fn(*args, **kwargs)
         except Exception as err:
             logger.exception(err)
+            return None
 
     return wrapper
